e5a3dc1198f799e7013bc655c18d4cf7e449c7d7
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-01-15 00:31:11 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import IntrotoBlogpost from './IntrotoBlogpost'
+import TopNavigation from './TopNavigation'
+import HeroImage from './HeroImage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <TopNavigation/>
+    <HeroImage/>
+    <IntrotoBlogpost/>
     </div>
   );
 }
---
+++ src/IntrotoBlogpost.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import BlogpostStyles from './css/BlogpostStyles.css'
+
+
+
+
+function IntrotoBlogpost() {
+    return (
+     
+     <div>
+       Hello World
+     </div>
+    
+    );
+  }
+  
+  export default IntrotoBlogpost;
+  
