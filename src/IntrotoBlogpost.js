c7ee60c5a2e9990382ceee41a5c3cf0e87dd3670
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-01-15 01:38:24 -0700

    add About Me Header and Image

--- src/IntrotoBlogpost.js
+++ src/IntrotoBlogpost.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap'
 import BlogpostStyles from './css/BlogpostStyles.css'
-
+import Me from './images/Me.jpg'
 
 
 
 function IntrotoBlogpost() {
     return (
      
-     <div>
-       Hello World
+     <div class="main-1">
+    <h1>ABOUT ME</h1>
+     <figure className="Me"> <img src={Me}></img></figure>
      </div>
     
     );
