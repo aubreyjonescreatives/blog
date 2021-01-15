acf35075a7400a45126d353567e7662c65940f4c
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-01-15 00:56:51 -0700

    add Header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css';
 import IntrotoBlogpost from './IntrotoBlogpost'
 import TopNavigation from './TopNavigation'
+import Header from './Header'
 import HeroImage from './HeroImage'
 
 function App() {
   return (
     <div className="App">
+    <Header/>
     <TopNavigation/>
     <HeroImage/>
     <IntrotoBlogpost/>
